f9378317f3874a476b340600d5f661121a70cdfd
PiotrJander <pjander2@gmail.com>
2016-09-15 11:48:28 -0400

    Third commit

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 1024
 
@@ -11,32 +11,109 @@
 #define STDERR 2
 
 
-// TODO rewrite atoi
-// TODO parse args
-//
+// TODO write Makefile
+// TODO write README
+// TODO check with valgrind
+// TODO run on speclab
+
+// TODO attribute outside sources
+// TODO answer questions about signals in README
 
 
+// ================
+// Global variables
+// ================
+
+/*
+ * Declared global so that it can be accessed by the alarm signal handler.
+ */
+static int pid;
+
+
+// ========
+// Typedefs
+// ========
+
+typedef void (*sighandler_t)(int);
+
+
+// =====================
+// Function declarations
+// =====================
+
+int atoi(const char* str);
 int shredder(int time_limit);
+size_t read_discard_overflow(char *buf, size_t nbyte);
 void handle_fork(char *input, size_t input_length, int time_limit);
-void parent(int pid, int time_limit);
+void parent(int time_limit);
+void alarm_handler(int signal);
 void child(char *input, size_t input_length);
-int get_path(char *path, char *input, size_t input_length);
+void get_path(char *path, char *input, size_t input_length);
+
+
+// System calls wrappers
 
 void write_wrapper(int fildes, const void *buf, size_t nbyte);
-ssize_t read_wrapper(int fildes, void *buf, size_t nbyte);
+size_t read_wrapper(int fildes, void *buf, size_t nbyte);
+int fork_wrapper();
+void wait_wrapper();
+void execve_wrapper(const char *path, char *const argv[]);
+void signal_wrapper(int signum, sighandler_t handler);
+void kill_wrapper(int pid, int sig);
 
+
+// ====================
+// Function definitions
+// ====================
 
 int main(int argc, char** argv) {
 
     int time_limit;
 
-    if (argc == 0) {
+    if (argc == 1) {
         time_limit = 0;
     } else {
         time_limit = atoi(argv[1]);
+        if (time_limit == 0) {
+            write(STDERR, "Invalid timeout argument.\n", 26);
+            _exit(1);
+        }
     }
 
     return shredder(time_limit);
+}
+
+
+/*
+ * Parses a string into a base 10 integer.
+ *
+ * Like stdlib's atoi, returns 0 for invalid inputs.
+ */
+int atoi(const char* str) {
+    // get the index of the last digit
+    int i = 0;
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    if (i == 0) {
+        // empty string
+        return 0;
+    }
+
+    // now we want to be looping back
+    int ret = 0;
+    int base = 1;
+    for (int j = i - 1; j >= 0 ; j--) {
+        int digit = str[j] - '0';
+        if (digit < 0 || digit > 9) {
+            // non-digit character
+            return 0;
+        }
+        ret += digit * base;
+        base *= 10;
+    }
+    return ret;
 }
 
 
@@ -48,7 +125,12 @@ int shredder(int time_limit) {
 
         write_wrapper(STDOUT, "shredder# ", 10);
 
-        ssize_t input_length = read_wrapper(STDIN, input, BUFFER_SIZE);
+        size_t input_length = read_discard_overflow(input, BUFFER_SIZE);
+
+        if (input_length == 0) {
+            write_wrapper(STDERR, "Input longer than 1024 bytes.\n", 30);
+            continue;
+        }
 
         handle_fork(input, input_length, time_limit);
 
@@ -57,45 +139,55 @@ int shredder(int time_limit) {
 
 
 /*
- * Controls the flow of execution depending on whether we are in the child
- * or parent process.
+ *
  */
-void handle_fork(char *input, ssize_t input_length, int time_limit) {
-    pid_t pid = fork();
-    switch (pid) {
-        case -1:
-            perror("fork");
-            _exit(1);
-        case 0:
-            child(input, input_length);
-        default:
-            parent(pid, time_limit);
+size_t read_discard_overflow(char *buf, size_t nbyte) {
+    size_t input_length = read_wrapper(STDIN, buf, nbyte);
+
+    if (input_length < nbyte || buf[nbyte - 1] == '\n') {
+        return input_length;
+    } else {
+        while(read_wrapper(STDIN, buf, nbyte) == input_length);
+        return 0;
     }
 }
 
 
-void parent(int pid, int time_limit) {
+/*
+ * Controls the flow of execution depending on whether we are in the child
+ * or parent process.
+ */
+void handle_fork(char *input, size_t input_length, int time_limit) {
 
-    void signal_handler(int signal) {
-        if (signal == SIGALRM) {
-            if (kill(pid, SIGKILL) == -1) {
-                perror("kill");
-                _exit(1);
-            }
-        }
+    // sets the global variable
+    pid = fork_wrapper();
+
+    if (pid == 0) {
+        child(input, input_length);
+    } else {
+        parent(time_limit);
     }
+}
 
-    if (signal(SIGALRM, signal_handler) == SIG_ERR) {
-        perror("signal");
-        _exit(1);
-    }
 
-    alarm(time_limit);
+void parent(int time_limit) {
 
-    int status = wait(NULL);
-    if (status == -1) {
-        perror("wait");
-        _exit(1);
+    signal_wrapper(SIGALRM, alarm_handler);
+
+    // apparently there are no exception to be handled with 'alarm'
+    alarm((unsigned int) time_limit);
+
+    wait_wrapper();
+
+    // since the child process has already terminated, we can cancel the alarm, if any
+    alarm(0);
+}
+
+
+void alarm_handler(int signal) {
+    if (signal == SIGALRM) {
+        kill_wrapper(pid, SIGKILL);
+        write_wrapper(STDOUT, "Bwahaha ... tonight I dine on turtle soup\n", 42);
     }
 }
 
@@ -107,23 +199,16 @@ void parent(int pid, int time_limit) {
  *
  * Otherwise, execute the command.
  */
-void child(char *input, ssize_t input_length) {
-    char path[input_length + 1];  // + 1 for null char;
-    int status = get_path(path, input, input_length);
+void child(char *input, size_t input_length) {
+    char path[input_length];
+    get_path(path, input, input_length);
 
-    if (status == -1) {
-        write(STDERR, "Input was empty or started with a space.\n", 41);
-        _exit(1);
-    }
+    // we hardcoded the first element of argv to be "foo"; it doesn't seem to matter anyway
+    char* argv[] = {"foo", NULL};
 
-    char* argv[] = {"ls", NULL};
-    char* envp[] = {NULL};
-    status = execve(path, argv, envp);
-    if (status == -1) {
-        perror("execve");
-        _exit(1);
-    }
+    execve_wrapper(path, argv);
 }
+
 
 
 /*
@@ -136,9 +221,9 @@ void child(char *input, ssize_t input_length) {
  *
  * -1 if the input has length 0 or begins with a space, 0 otherwise.
  */
-int get_path(char *path, char *input, ssize_t input_length) {
+void get_path(char *path, char *input, size_t input_length) {
     // first find number of characters until the first space
-    ssize_t path_size = input_length;
+    size_t path_size = input_length;
     for (size_t i = 0; i < input_length; ++i) {
         if (input[i] == ' ' || input[i] == '\n') {
             // at this point i equals the path size minus 1
@@ -148,18 +233,21 @@ int get_path(char *path, char *input, ssize_t input_length) {
     }
 
     if (path_size == 0) {
-        return -1;
+        write(STDERR, "Input was empty or started with a space.\n", 41);
+        _exit(1);
     }
 
     for (int j = 0; j < path_size; ++j) {
         path[j] = input[j];
     }
     path[path_size] = '\0';
-
-    write(STDOUT, path, path_size);
-
-    return 0;
 }
+
+
+// ====================
+// System call wrappers
+// ====================
+
 
 void write_wrapper(int fildes, const void *buf, size_t nbyte) {
     if (write(fildes, buf, nbyte) == -1) {
@@ -168,7 +256,8 @@ void write_wrapper(int fildes, const void *buf, size_t nbyte) {
     }
 }
 
-ssize_t read_wrapper(int fildes, void *buf, size_t nbyte) {
+
+size_t read_wrapper(int fildes, void *buf, size_t nbyte) {
     ssize_t status = read(fildes, buf, nbyte);
 
     switch (status) {
@@ -178,9 +267,47 @@ ssize_t read_wrapper(int fildes, void *buf, size_t nbyte) {
             perror("read");
             _exit(1);
         default:
-            return status;
+            return (size_t) status;
     }
 }
 
-// system call wrappers
 
+int fork_wrapper() {
+    int pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        _exit(1);
+    } else {
+        return pid;
+    }
+}
+
+
+void wait_wrapper() {
+    if (wait(NULL) == -1) {
+        perror("wait");
+        _exit(1);
+    }
+}
+
+void execve_wrapper(const char *path, char *const *argv) {
+    char* envp[] = {NULL};
+    if (execve(path, argv, envp) == -1) {
+        perror("execve");
+        _exit(1);
+    }
+}
+
+void signal_wrapper(int signum, sighandler_t handler) {
+    if (signal(signum, handler) == SIG_ERR) {
+        perror("signal");
+        _exit(1);
+    }
+}
+
+void kill_wrapper(int pid, int sig) {
+    if (kill(pid, sig) == -1) {
+        perror("kill");
+        _exit(1);
+    }
+}
--- tests.c
+++ tests.c
@@ -6,49 +6,67 @@
 int tests_run = 0;
 
 
-static char * test_get_path() {
-    char* input = "world ";
-    int input_length = 7;
-    char path[8];
-    get_path(path, input, input_length);
-    printf("%s\n", path);
-    mu_assert("test_get_path", strcmp(path, "world") == 0);
+//static char * test_get_path() {
+//    char* input = "hello ";
+//    size_t input_length = 7;
+//    char path[7];
+//    get_path(path, input, input_length);
+//    printf("%s\n", path);
+//    mu_assert("test_get_path", strcmp(path, "hello") == 0);
+//    return 0;
+//}
+//
+//static char * test_get_path_empty() {
+//    char* input = "";
+//    size_t input_length = 0;
+//    char path[1];
+//    get_path(path, input, input_length);
+//    mu_assert("test_get_path_invalid", strcmp(path, "") == 0);
+//    return 0;
+//}
+
+//static char * test_execute_command() {
+//    char* input = "";
+//    int input_length = 0;
+//    child(input, input_length);
+//    mu_assert("test_execute_command", 0);
+//    return 0;
+//}
+
+static char * test_parse_123() {
+    mu_assert("parse 123", atoi("123") == 123);
     return 0;
 }
 
-static char * test_get_path_invalid() {
-    char* input = "";
-    int input_length = 0;
-    char path[8];
-    int status = get_path(path, input, input_length);
-    mu_assert("test_get_path_invalid", status == -1);
+static char * test_parse_empty_string() {
+    mu_assert("parse empty", atoi("") == 0);
     return 0;
 }
 
-static char * test_execute_command() {
-    char* input = "";
-    int input_length = 0;
-    child(input, input_length);
-    mu_assert("test_execute_command", 0);
+static char * test_parse_invalid_string() {
+    mu_assert("parse invalid", atoi("12a3") == 0);
     return 0;
 }
 
 static char * all_tests() {
-    mu_run_test(test_get_path);
-    mu_run_test(test_get_path_invalid);
-    mu_run_test(test_execute_command);
+//    mu_run_test(test_get_path);
+//    mu_run_test(test_get_path_empty);
+//    mu_run_test(test_execute_command);
+    mu_run_test(test_parse_123);
+    mu_run_test(test_parse_empty_string);
+    mu_run_test(test_parse_invalid_string);
     return 0;
 }
 
-//int main() {
-//    char *result = all_tests();
-//    if (result != 0) {
-//        printf("%s\n", result);
-//    }
-//    else {
-//        printf("ALL TESTS PASSED\n");
-//    }
-//    printf("Tests run: %d\n", tests_run);
-//
-//    return result != 0;
-//}
+int main() {
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
